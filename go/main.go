ef66f25d0df1c569368ddfadfc05519b2034237f
Aleksandar Gujas <aleksandar.gujas@luxuryescapes.com>
2025-05-24 22:38:29 +1000

    feat: basic REPL for commands

--- go/main.go
+++ go/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"rsc.io/quote"
+	"os"
 )
 
+type InputBuffer struct {
+	buffer string
+}
+
+func newInputBuffer() *InputBuffer {
+	return &InputBuffer{
+		buffer: "",
+	}
+}
+
+func printPrompt() {
+	fmt.Print("db > ")
+}
+
+func readInput(inputBuffer *InputBuffer, scanner *bufio.Scanner) {
+	scanner.Scan()
+	inputBuffer.buffer = scanner.Text()
+}
+
 func main() {
-	fmt.Println(quote.Go())
+	inputBuffer := newInputBuffer()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		printPrompt()
+		readInput(inputBuffer, scanner)
+
+		// TODO what is this
+		if inputBuffer.buffer == ".exit" {
+			os.Exit(0)
+		} else {
+			fmt.Printf("Unrecognized command '%s'.\n", inputBuffer.buffer)
+		}
+	}
 }
